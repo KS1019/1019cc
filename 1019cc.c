e961f845acd64048c1e515a80673e96313283c41
Kotaro Suto <ng1a5ss43@gmail.com>
2019-07-03 15:17:57 +0000

    Add new code for putting error

--- 1019cc.c
+++ 1019cc.c
@@ -25,6 +25,23 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 /// エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...) {
@@ -111,6 +128,8 @@ int main(int argc, char **argv) {
     error("引数の個数が正しくありません");
     return 1;
   }
+
+  user_input = argv[1];
 
   // トークナイズする
   token = tokenize(argv[1]);
